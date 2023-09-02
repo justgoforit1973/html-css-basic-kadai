720cbcb5193b19a395fd53e6a930e99996f3f29f
justgoforit1973 <141423673+justgoforit1973@users.noreply.github.com>
2023-09-02 09:20:51 +0900

    Add files via upload

---
+++ kadai_019/event.js
@@ -0,0 +1,16 @@
+
+
+ 
+
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click' , () => {
+  const text = document.getElementById('text');
+  text.textContent  = "ボタンをクリックされました。";
+
+  // text.appendChild(text);
+
+});
+
+
